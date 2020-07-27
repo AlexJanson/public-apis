6767dafb986550cc9fcba3bce76a97d599954057
Alex Janson <Alex@hoofddorp.nl>
2020-07-27 23:25:03 +0200

    import svg like components and breakpoints defined

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div class="app">
+    <!-- <div class="nav">
       <router-link to="/">Home</router-link>
-    </div>
+    </div> -->
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import Navbar from '@/components/Navbar.vue';
+
+export default Vue.extend({
+  name: "App",
+  components: {
+    Navbar
+  }
+})
+</script>
+
 <style lang="scss">
 @import "~@/scss/fonts.scss";
+@import "~@/scss/mixins.scss";
 
 html,
 body {
   box-sizing: border-box;
+  background-color: #f4f4f4;
   font-family: 'Montserrat', Helvetica, Arial, sans-serif;
   margin: 0;
   padding: 0;
+}
+
+.app {
+  @include xl {
+    max-width: 1440px;
+  }
+
+  @include lg {
+    max-width: 992px;
+  }
+
+  margin: 0 auto;
+  padding: 0 20px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="nav-container">
+    <nav class="nav">
+      <HomeSvg class="home-svg" @click="onHome" />
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import HomeSvg from '@/assets/Home.svg';
+
+export default Vue.extend({
+  name: "Navbar",
+  components: {
+    HomeSvg
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    onHome() {
+      this.$router.push('/');
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.nav-container {
+  padding: 20px 0 0 0;
+
+  .home-svg {
+    height: 32px;
+    width: 32px;
+
+    cursor: pointer;
+  }
+}
+</style>
