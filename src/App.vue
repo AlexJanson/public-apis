3391d2b6c992ba3cabd7623c33d30dd69b665220
Alex Janson <Alex@hoofddorp.nl>
2020-07-27 20:38:55 +0200

    initial vue project setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import "~@/scss/fonts.scss";
+
+html,
+body {
+  box-sizing: border-box;
+  font-family: 'Montserrat', Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {
+  },
+  data() {
+    return {
+      msg: `Hello World!`
+    }
+  }
+};
+</script>
