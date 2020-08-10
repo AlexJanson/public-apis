e697c9616da72771cecc7f10d47325fb1a5e314b
Alex Janson <alex@hoofddorp.nl>
2020-08-10 17:14:57 +0200

    pagination refactoring

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -4,69 +4,51 @@
       <button class="prev" :disabled="currentPage === 1" @click="prev()">
         <LeftArrow class="arrow-left-svg" />
       </button>
-      <div class="pages">
-        <button :class="[currentPage === 1 ? 'active' : '']" @click="setCurrentPage(1)">1</button>
-        <p v-if="currentPage > 4">...</p>
-        <button :class="[currentPage === 2 ? 'active' : '']" v-else @click="setCurrentPage(2)">2</button>
 
+      <div v-if="isInBeginRange()" class="pages">
         <button
-          v-if="currentPage >= 4 && currentPage <= pages - 3"
-          @click="setCurrentPage(currentPage - 1)"
-        >{{ currentPage - 1 }}</button>
-        <button
-          :class="[currentPage === 3 ? 'active' : '']"
-          v-else-if="currentPage <= 4"
-          @click="setCurrentPage(3)"
-        >3</button>
-        <button
-          :class="[currentPage === pages - 4 ? 'active' : '']"
-          v-else
-          @click="setCurrentPage(pages - 4)"
-        >{{ pages - 4 }}</button>
-        <button
-          :class="[currentPage ? 'active' : '']"
-          v-if="currentPage >= 4 && currentPage <= pages - 3"
-        >{{ currentPage }}</button>
-        <button
-          :class="[currentPage === 4 ? 'active' : '']"
-          v-else-if="currentPage <= 4"
-          @click="setCurrentPage(4)"
-        >4</button>
-        <button
-          :class="[currentPage === pages - 3 ? 'active' : '']"
-          v-else
-          @click="setCurrentPage(pages - 3)"
-        >{{ pages - 3 }}</button>
-        <button
-          v-if="currentPage >= 4 && currentPage <= pages - 3"
-          @click="setCurrentPage(currentPage + 1)"
-        >{{ currentPage + 1 }}</button>
-        <button v-else-if="currentPage <= 4" @click="setCurrentPage(5)">5</button>
-        <button
-          :class="[currentPage === pages - 2 ? 'active' : '']"
-          v-else
-          @click="setCurrentPage(pages - 2)"
-        >{{ pages - 2 }}</button>
-
-        <p v-if="currentPage < pages - 3">...</p>
-        <button
-          :class="[currentPage === pages - 1 ? 'active' : '']"
-          v-else
-          @click="setCurrentPage(pages - 1)"
-        >{{ pages - 1 }}</button>
-        <button
-          :class="[currentPage === pages ? 'active' : '']"
-          @click="setCurrentPage(pages)"
-        >{{ pages }}</button>
+          :class="[currentPage === n ? 'active' : '']"
+          v-for="n in beginRange"
+          :key="n"
+          @click="setCurrentPage(n)"
+        >{{ n }}</button>
+        <button @click="setCurrentPage(5)">5</button>
+        <p>...</p>
+        <button @click="setCurrentPage(pages)">{{ pages }}</button>
       </div>
+
+      <div v-else-if="isInEndRange()" class="pages">
+        <button @click="setCurrentPage(1)">1</button>
+        <p>...</p>
+        <button @click="setCurrentPage(endRange[0] - 1)">{{ endRange[0] - 1 }}</button>
+        <button
+          :class="[currentPage === n ? 'active' : '']"
+          v-for="n in endRange"
+          :key="n"
+          @click="setCurrentPage(n)"
+        >{{ n }}</button>
+      </div>
+
+      <div v-else class="pages">
+        <button @click="setCurrentPage(pages)">1</button>
+        <p>...</p>
+        <button @click="setCurrentPage(currentPage - 1)">{{ currentPage - 1 }}</button>
+        <button class="active">{{ currentPage }}</button>
+        <button @click="setCurrentPage(currentPage + 1)">{{ currentPage + 1 }}</button>
+        <p>...</p>
+        <button @click="setCurrentPage(pages)">{{ pages }}</button>
+      </div>
+
       <button class="next" :disabled="currentPage === pages" @click="next()">
         <RightArrow class="arrow-right-svg" />
       </button>
     </div>
+
     <div v-else class="pagination-container">
       <button class="prev" :disabled="currentPage === 1" @click="prev()">
         <LeftArrow class="arrow-left-svg" />
       </button>
+
       <div class="pages">
         <button
           v-for="r in range"
@@ -75,6 +57,7 @@
           @click="setCurrentPage(r)"
         >{{ r }}</button>
       </div>
+
       <button class="next" :disabled="currentPage === pages" @click="next()">
         <RightArrow class="arrow-right-svg" />
       </button>
@@ -88,6 +71,8 @@ import { Component, Vue, Prop, Emit, Watch } from "vue-property-decorator";
 import LeftArrow from "@/assets/LeftArrow.svg";
 import RightArrow from "@/assets/RightArrow.svg";
 
+// Calculates the range of pages we have
+// from '1' to the total amount of pages
 const getRange = (n: number) => {
   const range: Array<number> = [];
 
@@ -105,48 +90,71 @@ const getRange = (n: number) => {
   },
 })
 export default class Pagination extends Vue {
+  // Total amount of pages we have to display
   @Prop({ type: Number, required: true })
   readonly pages!: number;
+  // The current page we are on
   @Prop(Number)
   readonly page!: number;
 
+  // This will hold all of the number from
+  // '1' to the 'total' amount of pages we have
   range: Array<number> = [];
-  beginRange: Array<number> = [1, 2, 3];
+  // Begin range will be the same no matter how many page we have
+  beginRange: Array<number> = [1, 2, 3, 4];
+  // End range for the last pages
   endRange: Array<number> = [];
+  // current page we are on
   currentPage = 1;
 
-  next() {
-    this.setCurrentPage(++this.currentPage);
-  }
-
-  prev() {
-    this.setCurrentPage(--this.currentPage);
-  }
-
-  @Emit("pageselect")
-  setCurrentPage(n: number) {
-    this.currentPage = n;
-  }
-
-  mounted() {
-    this.calculateRanges();
-  }
-
   @Watch("pages")
+  // If the amount of pages changes we calculate the ranges again
   onPagesChange() {
     this.calculateRanges();
   }
 
   @Watch("page")
+  // If the page prop changes we update the current page
   onPageChange() {
     this.setCurrentPage(this.page);
   }
 
+  @Emit("pageselect")
+  // Emitting an event when we navigate to another page
+  setCurrentPage(n: number) {
+    this.currentPage = n;
+  }
+
+  mounted() {
+    // Once the component is mounted we calculate the ranges
+    this.calculateRanges();
+  }
+
+  // Navigate to the next page
+  next() {
+    this.setCurrentPage(++this.currentPage);
+  }
+
+  // Navigate to the previous page
+  prev() {
+    this.setCurrentPage(--this.currentPage);
+  }
+
+  // Calculating the ranges of the end ranges for the ui to enable the '...' layout
   calculateRanges() {
     this.range = getRange(this.pages);
     this.endRange = [
-      ...this.range.slice(this.range.length - 3, this.range.length),
+      ...this.range.slice(this.range.length - 4, this.range.length),
     ];
+  }
+
+  // Check if the current page is inside the beginning range
+  isInBeginRange() {
+    return this.beginRange.includes(this.currentPage);
+  }
+  // Check if the current page is in the end range
+  isInEndRange() {
+    return this.endRange.includes(this.currentPage);
   }
 }
 </script>
@@ -155,7 +163,7 @@ export default class Pagination extends Vue {
 @import "~@/scss/colors";
 .pagination-container {
   display: flex;
-  max-width: 600px;
+  max-width: 400px;
   margin: 0 auto;
 
   .pages {
@@ -165,11 +173,11 @@ export default class Pagination extends Vue {
 
   .pages > button {
     &:not(:first-child) {
-      margin-left: 4px;
+      margin-left: 2px;
     }
 
     &:not(:last-child) {
-      margin-right: 4px;
+      margin-right: 2px;
     }
   }
 
